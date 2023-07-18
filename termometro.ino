34d830b444119b4fd2b388c4ea179f9b7be09bd9
felipeft_ <50221269+felipeft@users.noreply.github.com>
2023-07-18 10:48:09 -0300

    Update termometro.ino

--- termometro.ino
+++ termometro.ino
@@ -1,64 +1,104 @@
-/**
- * Copyright (c) 2015 panStamp S.L.U. <contact@panstamp.com>
- * 
- * This file is part of the panStamp project.
- * 
- * panStamp  is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation; either version 2 of the License, or
- * (at your option) any later version.
- * 
- * panStamp is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with panStamp; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301 
- * USA
- * 
- * Author: Daniel Berenguer
- * Creation date: 06/24/2015
- */
- 
-#include "thermistor.h"
-#include "HardwareSerial.h"
+#include <thermistor.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
+#include <stdio.h>
+#include <math.h>
 
-
+//NTC
 // Analog pin used to read the NTC
-#define NTC_PIN               A1
+#define NTC_PIN               A0
 
 // Thermistor object
 THERMISTOR thermistor(NTC_PIN,        // Analog pin
                       10000,          // Nominal resistance at 25 ºC
-                      3950,           // thermistor's beta coefficient
+                      3394,           // thermistor's beta coefficient
                       10000);         // Value of the series resistor
 
 // Global temperature reading
 uint16_t temp;
 
+
+//resistência
 /**
- * setup
- *
- * Arduino setup function
- */
-void setup()
-{
-  Serial.begin(9600);
+  Obtém a resistência de uma porta analógica informada do microcontrolador. Para isso,
+  deve-se estar conectado  um resistor em série na montagem de um divisor de tensão.
+
+  @param pin - O pino da porta à ser medida a resistência.
+  @param voltageUc - A tensão nominal do microcontrolador.
+  @param adcResolutionUc - O valor máximo da saída do
+    ADC (Arduino UNO = 1024.0 | ESP32 = 4095.0).
+  @param resistenciaEmSerie - A resistência do resistor em série ao qual
+    está conectado à porta analógica.
+
+  @return a resistência, em OHM, obtida.
+*/
+float getResistencia(int pin, float voltageUc, float adcResolutionUc, float resistenciaEmSerie) {
+  float resistenciaDesconhecida = 0;
+
+  resistenciaDesconhecida =
+    resistenciaEmSerie *
+    (voltageUc /
+     (
+       (analogRead(pin) * voltageUc) /
+       adcResolutionUc
+     ) - 1
+    );
+
+  return resistenciaDesconhecida;
 }
 
-/**
- * loop
- *
- * Arduino main loop
- */
-void loop()
-{
+
+//reverse steinhart
+float calculateR(double T, double T0, double B, double R0) {
+    double result = R0 * exp(B * (1 / T - 1 / T0));
+    return result;
+}
+
+void ReverseSteinhart(double T) {
+    
+    double T0 = 25, B = 3394, R0 = 10000;
+    T += 273.15; 
+    T0 += 273.15; 
+    double R = calculateR(T, T0, B, R0);
+
+    Serial.print(R);
+}
+
+
+//Dsb
+#define ONE_WIRE_BUS 10
+
+OneWire oneWire(ONE_WIRE_BUS);
+DallasTemperature sensors(&oneWire);
+DeviceAddress insideThermometer = { 0x28, 0x66, 0x14, 0x57, 0x04, 0xE1, 0x3C, 0x60 };
+
+void printTemperature(DeviceAddress deviceAddress){
+  float tempC = sensors.getTempC(deviceAddress);
+  if (tempC == -127.00) {
+    Serial.print("Erro de leitura");
+  } 
+  else {
+    Serial.print("DS18B20: "); 
+    Serial.print(tempC);
+    Serial.println(" *C");
+    Serial.print("Resistencia esperada: ");
+    ReverseSteinhart(tempC);
+    Serial.print("\r\n");
+  }
+}
+
+//main
+void setup(){
+  Serial.begin(9600);
+  sensors.begin();      //inicializa sensores
+  sensors.setResolution(insideThermometer, 12); //configura para resolução de 10 bits
+}
+
+void loop(){
+  sensors.requestTemperatures();
+  printTemperature(insideThermometer);
   temp = thermistor.read();   // Read temperature
-
-  Serial.print("Temp in 1/10 ºC : ");
-  Serial.println(temp);
-
-  delay(5000);
+  Serial.print("\r\n\r\n");
+  
+  delay(500);
 }
